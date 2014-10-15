7a2e449c93235490cacb8f798a99aa1dd8284248
Boxing <boxingp@gmail.com>
2014-10-15 16:41:26 +0530

    Input is empty will return empty.

---
+++ src/com/twu/mommy/Mommy.java
@@ -0,0 +1,10 @@
+package com.twu.mommy;
+
+/**
+ * Created by bxpeng on 10/15/14.
+ */
+public class Mommy {
+    public String mommyfy(String input) {
+        return "";
+    }
+}
---
+++ test/com/twu/mommy/MommyTest.java
@@ -0,0 +1,17 @@
+package com.twu.mommy;
+
+import org.junit.Test;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+/**
+ * Created by bxpeng on 10/15/14.
+ */
+public class MommyTest {
+    @Test
+    public void shouldReturnEmptyWhenInputIsEmpty() {
+        Mommy mommy = new Mommy();
+        assertThat(mommy.mommyfy(""), is(""));
+    }
+}
